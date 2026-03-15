f6f255db73ac778e8b20e03c1f82a677c0dd0875
Ankit-Dhankhar <ankit97dhankhar@gmail.com>
2026-03-15 16:58:55 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -73,15 +73,20 @@ fn parse_arguments(input: &str) -> Vec<String> {
     let mut args = Vec::new();
     let mut current = String::new();
     let mut in_single_quotes = false;
+    let mut in_double_quotes = false;
     let mut token_in_progress = false;
 
     for ch in input.chars() {
         match ch {
-            '\'' => {
+            '\'' if !in_double_quotes => {
                 in_single_quotes = !in_single_quotes;
                 token_in_progress = true;
             }
-            _ if ch.is_whitespace() && !in_single_quotes => {
+            '"' if !in_single_quotes => {
+                in_double_quotes = !in_double_quotes;
+                token_in_progress = true;
+            }
+            _ if ch.is_whitespace() && !in_single_quotes && !in_double_quotes => {
                 if token_in_progress {
                     args.push(std::mem::take(&mut current));
                     token_in_progress = false;
