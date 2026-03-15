0714af7c09079d3516533d8a41f58cbe5e5b2555
Ankit-Dhankhar <ankit97dhankhar@gmail.com>
2026-03-15 11:54:55 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{self, Write};
 use std::process;
 
 const PROMPT: &str = "$ ";
+const BUILTINS: [&str; 3] = ["echo", "exit", "type"];
 
 fn main() {
     if let Err(e) = run_shell() {
@@ -14,9 +15,16 @@ fn run_shell() -> io::Result<()> {
     loop {
         print_prompt()?;
 
-        let input = read_input()?;
+        let input = match read_input()? {
+            Some(input) => input,
+            None => break,
+        };
 
         let command = input.trim();
+
+        if command.is_empty() {
+            continue;
+        }
 
         if !execute_command(command) {
             break;
@@ -31,14 +39,19 @@ fn print_prompt() -> io::Result<()> {
 }
 
 
-fn read_input() -> io::Result<String> {
+fn read_input() -> io::Result<Option<String>> {
     let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
-    Ok(input)
+    let bytes_read = io::stdin().read_line(&mut input)?;
+    if bytes_read == 0 {
+        return Ok(None);
+    }
+    Ok(Some(input))
 }
 
 fn execute_command(command: &str) -> bool {
     let parts: Vec<&str> = command.split_whitespace().collect();
+
+
 
     match parts.first() {
         Some(&"exit") => {
@@ -47,6 +60,13 @@ fn execute_command(command: &str) -> bool {
         Some(&"echo") => {
             if let Some(rest) = command.strip_prefix("echo").map(|s| s.trim()) {
                 println!("{}", rest);
+            }
+        }
+        Some(&"type") => {
+            if BUILTINS.contains(&parts[1]) {
+                println!("{} is a shell builtin", parts[1]);
+            } else {
+                println!("{}: not found", parts[1]);
             }
         }
         _ => {
